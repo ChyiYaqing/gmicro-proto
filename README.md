ff277265a642a88c20d837bcfe8f86b7fc6f16d7
chyiyaqing <chyiyaqing@gmail.com>
2024-12-12 14:28:01 +0800

    add some comment

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # gmicro-proto
+
+创建`golang/order/v0.0.1`标签tag
+
+```bash
+./protoc.sh order v0.0.1 chyiyaqing chyiyaqing@gmail.com
+```
+
+创建`golang/payment/v0.0.1`标签tag
+
+```bash
+./protoc.sh payment v0.0.1 chyiyaqing chyiyaqing@gmail.com
+```
+
+创建`golang/shipping/v0.0.1`标签tag
+
+```bash
+./protoc.sh shipping v0.0.1 chyiyaqing chyiyaqing@gmail.com
+```
--- protoc.sh
+++ protoc.sh
@@ -9,10 +9,12 @@ git config user.name "$USER_NAME"
 git config user.email "$EMAIL"
 git fetch --all && git checkout main
 
+# 安装依赖
 sudo apt-get install -y protobuf-compiler golang-goprotobuf-dev
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 
+# 生成protobuf代码,将.proto文件编译成Go代码，包括gRPC服务代码
 protoc \
     --go_out=./golang \
     --go_opt=paths=source_relative \
@@ -20,11 +22,18 @@ protoc \
     --go-grpc_opt=paths=source_relative \
     ./${SERVICE_NAME}/*.proto
 
+# 初始化Go 模块
 cd golang/${SERVICE_NAME}
 go mod init github.com/chyiyaqing/gmicro-proto/golang/${SERVICE_NAME} || true
 go mod tidy
+
 cd ../../
 git add . && git commit -am "proto update" || true
 git push origin HEAD:main
+
+# 版本标签管理
+# -f: force 强制创建标签，如果标签存在则覆盖
+# -a: annotated 创建一个带注释的标签
+# -m: 指定标签的描述信息
 git tag -fa golang/${SERVICE_NAME}/${RELEASE_VERSION} -m "golang/${SERVICE_NAME}/${RELEASE_VERSION}"
 git push origin refs/tags/golang/${SERVICE_NAME}/${RELEASE_VERSION}
