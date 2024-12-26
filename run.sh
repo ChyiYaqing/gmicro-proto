aeff5ceae28986f4639d02eb961682acfea3173e
chyiyaqing <chyiyaqing@gmail.com>
2024-12-26 14:08:26 +0800

    add GOPATH/bin to PATH

--- run.sh
+++ run.sh
@@ -16,8 +16,10 @@ go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2@latest
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest 
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 
-# grpc-gateway add in path
-source PATH=$PATH:/usr/local/go/bin
+echo "Checking installed binaries:"
+ls -l $(go env GOPATH)/bin/
+which protoc-gen-go
+which protoc-gen-grpc-gateway
 
 # 生成protobuf代码,将.proto文件编译成Go代码，包括gRPC服务代码
 protoc \
